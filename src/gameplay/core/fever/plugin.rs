1110a42d548cb9c339138ad7114ca7e4f7997aee
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2026-02-08 11:07:01 +0100

    Update plugin.rs

--- src/gameplay/core/fever/plugin.rs
+++ src/gameplay/core/fever/plugin.rs
@@ -4,9 +4,7 @@ pub struct FeverPlugin;
 
 impl Plugin for FeverPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<GlobalTemperature>().add_systems(
-            FixedUpdate,
-            (tick_source, tick_temp, tick_fever).chain(),
-        );
+        app.init_resource::<GlobalTemperature>()
+            .add_systems(FixedUpdate, (tick_source, tick_temp, tick_fever).chain());
     }
 }
