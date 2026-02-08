3bcf550dc079182c81915f57d75f805e5049b40a
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2026-02-08 13:05:18 +0100

    docs, naming, cleanup, stomach

--- src/gameplay/core/fever/plugin.rs
+++ src/gameplay/core/fever/plugin.rs
@@ -1,10 +1,15 @@
-use crate::gameplay::core::{fever::systems::*, temperature::systems::*, *};
+use bevy::app::prelude::*;
+
+use crate::gameplay::core::{fever::systems::*, temperature::*, *};
 
 pub struct FeverPlugin;
 
 impl Plugin for FeverPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<GlobalTemperature>()
-            .add_systems(FixedUpdate, (tick_source, tick_temp, tick_fever).chain());
+        if !app.is_plugin_added::<TemperaturePlugin>() {
+            app.add_plugins(TemperaturePlugin);
+        }
+
+        app.add_systems(FixedUpdate, (source, fever).chain());
     }
 }
