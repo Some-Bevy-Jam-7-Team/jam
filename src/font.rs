c2986c383843e0d701e6612b36fc14f65786d58f
Joona Aalto <jondolf.dev@gmail.com>
2026-02-09 02:18:03 +0200

    Use const asset handles

--- src/font.rs
+++ src/font.rs
@@ -1,34 +1,35 @@
 //! Font assets.
 
 use bevy::{
+	asset::{load_internal_binary_asset, uuid_handle},
 	ecs::{lifecycle::HookContext, world::DeferredWorld},
 	prelude::*,
 };
 
+pub(crate) const DEFAULT_FONT: Handle<Font> = uuid_handle!("28dfb1e9-7b35-454f-9fe3-3457797c40bc");
+
 pub(crate) fn plugin(app: &mut App) {
-	app.register_type::<FontAssets>();
+	load_internal_binary_asset!(
+		app,
+		DEFAULT_FONT,
+		"../assets/fonts/Finger_Paint/FingerPaint-Regular.ttf",
+		load_font
+	);
+
 	app.world_mut()
 		.register_component_hooks::<TextFont>()
 		.on_add(on_add_text);
-
-	let assets = app.world().resource::<AssetServer>();
-	app.insert_resource(FontAssets {
-		default: assets.load("fonts/Finger_Paint/FingerPaint-Regular.ttf"),
-	});
 }
 
-#[derive(Resource, Clone, Debug, Reflect)]
-#[reflect(Resource)]
-pub(crate) struct FontAssets {
-	pub default: Handle<Font>,
+fn load_font(bytes: &[u8], _path: String) -> Font {
+	Font::try_from_bytes(bytes.to_vec()).unwrap()
 }
 
 fn on_add_text(mut world: DeferredWorld, ctx: HookContext) {
-	let default_font = world.resource::<FontAssets>().default.clone();
 	let mut font = world.get_mut::<TextFont>(ctx.entity).unwrap();
 
 	// If the font is the default handle, replace it with our default font.
 	if font.font == Handle::default() {
-		font.font = default_font;
+		font.font = DEFAULT_FONT;
 	}
 }
