b109f3b93c6cf804178e257f530a759035c2e669
Jan Hohenheim <jan@hohenheim.ch>
2026-02-14 22:14:25 +0100

    Shorm (#99)

--- src/props/specific/mushroom.rs
+++ src/props/specific/mushroom.rs
@@ -7,7 +7,7 @@ use crate::props::setup::{setup_static_prop_with_convex_hull, static_bundle};
 use crate::scatter::layers::MushroomLayer;
 use crate::third_party::bevy_trenchbroom::GetTrenchbroomModelPath;
 
-use avian3d::prelude::ColliderConstructor;
+use avian3d::prelude::{ColliderConstructor, ColliderConstructorHierarchy, LockedAxes, RigidBody};
 use bevy::prelude::*;
 use bevy_feronia::prelude::ScatteredInstance;
 use bevy_trenchbroom::prelude::ReflectQuakeClass;
@@ -70,6 +70,10 @@ pub fn scattered_shroom(
 					completes_subobjective: Some("leave".to_string()),
 					interaction_relay: None,
 				},
+				ColliderConstructorHierarchy::new(ColliderConstructor::ConvexHullFromMesh)
+					.with_default_density(200.0),
+				LockedAxes::new().lock_rotation_x().lock_rotation_z(),
+				RigidBody::Dynamic,
 			));
 		} else {
 			cmd.entity(trigger.entity).insert(Mushroom);
