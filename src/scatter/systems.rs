f756f8cdd2cff24f6cb55f2c950fd67182ba5f06
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2026-02-14 03:11:50 +0100

    Update systems.rs

--- src/scatter/systems.rs
+++ src/scatter/systems.rs
@@ -1,7 +1,6 @@
 use crate::gameplay::level::{CurrentLevel, LevelAssets};
 use crate::scatter::{components::*, layers::*};
 
-use crate::screens::Screen;
 use bevy::asset::{AssetEvent, Assets};
 use bevy::image::Image;
 use bevy::pbr::StandardMaterial;
@@ -16,32 +15,22 @@ pub fn clear_scatter_root(mut cmd: Commands, root: Single<Entity, With<ScatterRo
 pub fn spawn_scatter_layers(
 	mut cmd: Commands,
 	landscape: Single<Entity, With<ScatterRoot>>,
-	current_level: Res<CurrentLevel>,
 ) {
 	let landscape = landscape.into_inner();
 	debug!("Spawning scatter layers...");
-	match *current_level {
-		CurrentLevel::DayOne => {}
-		CurrentLevel::DayTwo => {
-			cmd.spawn((
-				DespawnOnExit(Screen::Gameplay),
-				RockLayer,
-				ChildOf(landscape),
-			));
-			cmd.spawn((
-				DespawnOnExit(Screen::Gameplay),
-				MushroomLayer,
-				ChildOf(landscape),
-			));
-			cmd.spawn((
-				DespawnOnExit(Screen::Gameplay),
-				GrassLayer,
-				ChildOf(landscape),
-			));
-		}
-		CurrentLevel::Train => {}
-		CurrentLevel::Karoline => {}
-	}
+
+	cmd.spawn((
+		RockLayer,
+		ChildOf(landscape),
+	));
+	cmd.spawn((
+		MushroomLayer,
+		ChildOf(landscape),
+	));
+	cmd.spawn((
+		GrassLayer,
+		ChildOf(landscape),
+	));
 }
 
 pub fn scatter(
