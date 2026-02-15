9c0d90f9cfd06634ae4f6e92a068b6317197b1dd
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2026-02-15 05:22:56 +0100

    fix race condition

--- src/asset_tracking.rs
+++ src/asset_tracking.rs
@@ -75,15 +75,17 @@ impl ResourceHandles {
 fn load_resource_assets(world: &mut World) {
 	world.resource_scope(|world, mut resource_handles: Mut<ResourceHandles>| {
 		world.resource_scope(|world, assets: Mut<AssetServer>| {
-			for _ in 0..resource_handles.waiting.len() {
-				let (handle, insert_fn) = resource_handles.waiting.pop_front().unwrap();
-				if assets.is_loaded_with_dependencies(&handle) {
-					insert_fn(world, &handle);
-					resource_handles.finished.push(handle);
-				} else {
-					resource_handles.waiting.push_back((handle, insert_fn));
-				}
-			}
+            let mut waiting = VecDeque::new();
+            while let Some((handle, insert_fn)) = resource_handles.waiting.pop_front() {
+                if assets.is_loaded_with_dependencies(&handle) {
+                    insert_fn(world, &handle);
+                    resource_handles.finished.push(handle);
+                } else {
+                    waiting.push_back((handle, insert_fn));
+                }
+            }
+
+            resource_handles.waiting = waiting;
 		});
 	});
 }
