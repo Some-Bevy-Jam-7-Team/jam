ddf794fcb3fe47b361c22dd6267754f57c8dd551
Jan Hohenheim <jan@hohenheim.ch>
2026-02-16 07:25:52 +0100

    Revert "shader, mushroom tweak (#167)"
    This reverts commit 0c7ce034b21cd41d3b695dab45714b6ed65e0e4d.

--- src/props/specific/mushroom.rs
+++ src/props/specific/mushroom.rs
@@ -73,7 +73,7 @@ pub(crate) struct MushroomModel5;
 
 #[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
-#[require(EnvironmentTemperature(41.))]
+#[require(EnvironmentTemperature)]
 pub(crate) struct Mushroom;
 
 fn setup_mushroom<T: Component + QuakeClass>(
