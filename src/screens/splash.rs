da0bc388aea3eec9cec3bc9ae69b3846c121d05b
Jan Hohenheim <jan@hohenheim.ch>
2026-02-16 04:35:15 +0100

    Rename

--- src/screens/splash.rs
+++ src/screens/splash.rs
@@ -58,7 +58,7 @@ fn spawn_splash_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
 				width: Val::Percent(70.0),
 				..default()
 			},
-			ImageNode::new(asset_server.load("embedded://jam/screens/files/splash.png")),
+			ImageNode::new(asset_server.load("embedded://laborgeist/screens/files/splash.png")),
 			ImageNodeFadeInOut {
 				total_duration: SPLASH_DURATION_SECS,
 				fade_duration: SPLASH_FADE_DURATION_SECS,
